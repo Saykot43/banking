8e44e188fa4f9c8bd357838c2c84aa5f4a83cadd
Saykot Kumar Barmon <saykot.cse43@gmail.com>
2022-02-10 16:41:01 +0600

    deposit, withdraw & Balace add use grid

--- js/bank.js
+++ js/bank.js
@@ -1,8 +1,12 @@
+// get the submit button
 document.getElementById('login-submit').addEventListener('click', function() {
+    // get email
     const emailField= document.getElementById('email');
     const userEmail = emailField.value;
     // console.log(userEmail);
+    // get password
     const passwordField = document.getElementById('password').value;
+    // check email & password
     if(userEmail=='admin@admin.com' && passwordField == 'admin'){
         window.location.href ='banking.html'
     }
